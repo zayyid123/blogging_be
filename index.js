f03ae9be28c77f3c83a3e9b8d81335ca9b3bf5df
zayyid123 <mochamadzayyid@gmail.com>
2024-11-15 13:37:58 +0700

    init server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello")
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+
+// Middleware untuk parsing request body
+app.use(bodyParser.json());
+
+// Jalankan server
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
